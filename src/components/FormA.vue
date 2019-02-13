672bfc8cfa63dd0c55d8a4e0aef246066141e261
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2019-02-13 14:20:57 -0800

    First commit

---
+++ src/components/FormA.vue
@@ -0,0 +1,45 @@
+<template>
+  　<div class="hello">
+    <h1>FormA</h1>
+    <p><input type="text" v-model="name" placeholder="input your name"></p>
+    <p><input type="text" v-model="msg" placeholder="input message"></p>
+    <p>Name:{{ name }}</p>
+    <p>Message:{{ msg }}</p>
+    <button v-on:click="check">Check</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormA',
+  data () {
+    return {
+      name: '',
+      msg: ''
+    }
+  },
+  methods: {
+    check () {
+      alert('Your name is ' + this.name)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/FormB.vue
@@ -0,0 +1,45 @@
+<template>
+  　<div class="hello">
+    <h1>FormB</h1>
+    <p><input type="text" v-model="age" placeholder="input your age"></p>
+    <p><input type="text" v-model="address" placeholder="input your address"></p>
+    <p>Age:{{ age }}</p>
+    <p>Address:{{ address }}</p>
+    <button v-on:click="check">Check</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormB',
+  data () {
+    return {
+      age: '',
+      address: ''
+    }
+  },
+  methods: {
+    check () {
+      alert('Your age is ' + this.age + '\nYour address is ' + this.address)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
